6aa94df2e1e893e6cf88e282d0d965e6e8e512dc
William Adams <wadams.dev@icloud.com>
2023-08-27 17:15:42 -0400

    Adding Project 1 initial files including images

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by William Adams on 8/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
