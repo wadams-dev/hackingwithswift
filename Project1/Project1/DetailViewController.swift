b511d97bcdb2264786a4d8d0a06a3dd7444b819b
William Adams <wadams.dev@icloud.com>
2023-08-28 22:52:33 -0400

    Added code that loads images into imageView on DetailViewController

--- Project1/Project1/DetailViewController.swift
+++ Project1/Project1/DetailViewController.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
     @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
     }
 }
