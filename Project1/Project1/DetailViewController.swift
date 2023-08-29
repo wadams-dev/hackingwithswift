6a70c49b5791497b237cd22b961a241a94108c15
William Adams <wadams.dev@icloud.com>
2023-08-28 22:33:10 -0400

    Added detail view controller

---
+++ Project1/Project1/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by William Adams on 8/28/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
