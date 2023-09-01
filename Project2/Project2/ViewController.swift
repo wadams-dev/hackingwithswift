62fbcdcfa10ca30f5bec884e991c905240b41bf8
William Adams <wadams.dev@icloud.com>
2023-08-31 22:23:30 -0400

    Initial project set up

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by William Adams on 8/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
